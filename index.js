0fcc0d7bae3063b8225c729335078ae793f862af
ChungGold <chungau@csu.fullerton.edu>
2021-04-02 20:02:16 -0700

    randomized keyframes

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-let container = document.getElementById('container');
+const keyframes = document.createElement('style');
+
+const container = document.getElementById('container');
 
 window.addEventListener('keypress', (event) => {
-    let num = Math.round(Math.random()*2);
-    
+// Keyframes
+    keyframes.innerHTML = `
+    @keyframes rain {
+        0% {
+            transform: scale(0.8, 1.2) translateY(-800px);
+        }
+        50% {
+            transform: scale(1.4, 0.6);
+        }
+        70% {
+            transform: scale(0.9, 1.1) 
+            translateY(${-100 - Math.round(Math.random() * 100)}px) 
+            translateX(${Math.round(Math.random() * 300) - 150}px) scale(0);
+        }
+        100% {
+            transform: scale(0.9, 1.1) 
+            translateY(${-100 - Math.round(Math.random() * 0)}px) 
+            translateX(${Math.round(Math.random() * 300) - 150}px) scale(0);
+        }
+    }
+    `
+    document.body.appendChild(keyframes);
+
+// Typing
     const letter = document.createElement('span');
     
-    letter.style.animation = `1 normal ease-in-out forwards rain${num} ${1000+Math.random()*500}ms`
+    letter.style.animation = `1 normal linear forwards rain ${1200 + Math.round(Math.random() * 800)}ms`
 
     letter.innerHTML = `${event.key}`
 
-    letter.style.left = `${Math.round(Math.random()*100)}%`
-    
+    letter.style.left = `${Math.round(Math.random() * 100)}%`
+
     container.appendChild(letter);
 });
 
